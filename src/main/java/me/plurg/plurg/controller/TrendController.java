a82ce5b896cc598c9ad6fa513220946ca8bb17fa
ConfidenceDev <confidostic3@gmail.com>
2023-06-15 20:38:42 +0100

    Fix 3

--- src/main/java/me/plurg/plurg/controller/TrendController.java
+++ src/main/java/me/plurg/plurg/controller/TrendController.java
@@ -19,6 +19,11 @@ public class TrendController {
     @Autowired
     private TrendService trendService;
 
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!!!!";
+    }
+
     @GetMapping("/trends/{page}/{size}")
     public ResponseEntity<Page<Trend>> getTrend(@PathVariable int page, @PathVariable int size){
         log.info("Retrieving items");
